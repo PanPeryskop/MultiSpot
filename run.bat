176fa7945be37a4c1d27af052a9bf8b208fc2ab8
Pan Peryskop <134305178+PanPeryskop@users.noreply.github.com>
2024-09-04 15:12:53 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,34 @@
 @echo off
 cd %~dp0
+
+git --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Git is not installed. Please install Git to continue.
+    exit /b 1
+)
+
+set REMOTE_URL=https://github.com/PanPeryskop/MultiSpot
+
+git remote get-url origin >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Setting the remote repository URL...
+    git remote add origin %REMOTE_URL%
+) else (
+    echo Verifying the remote repository URL...
+    git remote set-url origin %REMOTE_URL%
+)
+
+echo Checking if you have the latest version...
+git fetch
+
+setlocal enabledelayedexpansion
+for /f "tokens=*" %%i in ('git rev-list HEAD...origin/main --count') do set NEW_COMMITS=%%i
+if !NEW_COMMITS! neq 0 (
+    echo A new version is available. Updating...
+    git pull
+) else (
+    echo The application is up to date.
+)
+
 pythonw main.pyw
 exit
